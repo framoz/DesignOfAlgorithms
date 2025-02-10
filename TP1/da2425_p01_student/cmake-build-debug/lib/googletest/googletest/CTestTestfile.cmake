4b72da8f67450b66a6deb26a7de1394e146481d0
framoz <felipe.bueno.ramos@gmail.com>
2025-02-10 21:23:58 +0000

    CLion debug files

---
+++ TP1/da2425_p01_student/cmake-build-debug/lib/googletest/googletest/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/framoz/Documents/FEUP_2425/2s/DA/TP1/da2425_p01_student/lib/googletest/googletest
+# Build directory: /home/framoz/Documents/FEUP_2425/2s/DA/TP1/da2425_p01_student/cmake-build-debug/lib/googletest/googletest
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
