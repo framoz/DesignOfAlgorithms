b24e7087a4c17d92dd1014be73a0a9dced176d3b
framoz <felipe.bueno.ramos@gmail.com>
2025-02-11 10:48:36 +0000

    Exercise 2

--- TP1/da2425_p01_student/TP1/ex2.cpp
+++ TP1/da2425_p01_student/TP1/ex2.cpp
@@ -2,6 +2,12 @@
 #include "../data_structures/Graph.h"
 
 using namespace std;
+#include <queue>
+
+/*
+ * Ex2 a. 1, 2, 5, 4, 3, 7, 6
+ *        1, 2, 5, 4, 7, 3, 6
+ */
 
 /*
  * Performs a topological sorting of the vertices of a graph.
@@ -11,6 +17,46 @@ using namespace std;
 template <typename T>
 vector<T> topsort(Graph<T> *g) {
     vector<int> res;
-    // TODO
+    vector<Vertex<T> *> vSet = g->getVertexSet();
+
+    for (auto v : vSet)
+        v->setIndegree(0);
+
+    for (auto v : vSet) {
+        for (auto edge: v->getAdj()) {
+            unsigned int indegree = edge->getDest()->getIndegree();
+            edge->getDest()->setIndegree(indegree+1);
+            }
+    }
+
+    queue<Vertex<T> *> q;
+    for (auto vertex: vSet) {
+        if (vertex->getIndegree() == 0) {
+            q.push(vertex);
+        }
+    }
+
+
+    while (!q.empty()) {
+        Vertex<T> *u = q.front();
+        q.pop();
+        res.push_back(u->getInfo());
+        for (auto edge : u->getAdj()) {
+            Vertex<T> *neighbor = edge->getDest();
+
+            neighbor->setIndegree(neighbor->getIndegree() - 1);
+
+            if (neighbor->getIndegree() == 0) {
+                q.push(neighbor);
+            }
+        }
+    }
+
+    if (res.size() != vSet.size()) {
+        return {};
+    }
     return res;
 }
+
+
+
