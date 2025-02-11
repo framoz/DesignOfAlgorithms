bdc9ec419eaa5951f591aadd596423f3d0cd278f
framoz <felipe.bueno.ramos@gmail.com>
2025-02-11 10:50:18 +0000

    Exercise 3

--- TP1/da2425_p01_student/TP1/ex3.cpp
+++ TP1/da2425_p01_student/TP1/ex3.cpp
@@ -14,7 +14,45 @@ using namespace std;
 
 template <class T>
 bool isDAG(Graph<T> *g) {
-    // TODO
+    vector<int> res;
+    vector<Vertex<T> *> vSet = g->getVertexSet();
+
+    for (auto v : vSet)
+        v->setIndegree(0);
+
+    for (auto v : vSet) {
+        for (auto edge: v->getAdj()) {
+            unsigned int indegree = edge->getDest()->getIndegree();
+            edge->getDest()->setIndegree(indegree+1);
+        }
+    }
+
+    queue<Vertex<T> *> q;
+    for (auto vertex: vSet) {
+        if (vertex->getIndegree() == 0) {
+            q.push(vertex);
+        }
+    }
+
+
+    while (!q.empty()) {
+        Vertex<T> *u = q.front();
+        q.pop();
+        res.push_back(u->getInfo());
+        for (auto edge : u->getAdj()) {
+            Vertex<T> *neighbor = edge->getDest();
+
+            neighbor->setIndegree(neighbor->getIndegree() - 1);
+
+            if (neighbor->getIndegree() == 0) {
+                q.push(neighbor);
+            }
+        }
+    }
+
+    if (res.size() != vSet.size()) {
+        return false;
+    }
     return true;
 }
 
