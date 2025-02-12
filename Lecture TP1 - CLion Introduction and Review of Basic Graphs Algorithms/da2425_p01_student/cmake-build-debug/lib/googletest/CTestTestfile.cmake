0d946f4f6b146618a37414465c452c9c61725af3
framoz <felipe.bueno.ramos@gmail.com>
2025-02-12 17:31:57 +0000

    Changed dir name

---
+++ Lecture TP1 - CLion Introduction and Review of Basic Graphs Algorithms/da2425_p01_student/cmake-build-debug/lib/googletest/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /home/framoz/Documents/FEUP_2425/2s/DA/Lecture TP1 - CLion Introduction and Review of Basic Graphs Algorithms/da2425_p01_student/lib/googletest
+# Build directory: /home/framoz/Documents/FEUP_2425/2s/DA/Lecture TP1 - CLion Introduction and Review of Basic Graphs Algorithms/da2425_p01_student/cmake-build-debug/lib/googletest
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("googlemock")
--- TP1/da2425_p01_student/cmake-build-debug/lib/googletest/googlemock/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/framoz/Documents/FEUP_2425/2s/DA/TP1/da2425_p01_student/lib/googletest/googlemock
-# Build directory: /home/framoz/Documents/FEUP_2425/2s/DA/TP1/da2425_p01_student/cmake-build-debug/lib/googletest/googlemock
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("../googletest")
