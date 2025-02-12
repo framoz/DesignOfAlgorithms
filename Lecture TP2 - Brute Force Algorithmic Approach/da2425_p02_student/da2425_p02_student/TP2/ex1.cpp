36315e24e8cdc18ee7e518505017cae7dd027cfc
framoz <felipe.bueno.ramos@gmail.com>
2025-02-12 17:33:09 +0000

    Added TP2

---
+++ Lecture TP2 - Brute Force Algorithmic Approach/da2425_p02_student/da2425_p02_student/TP2/ex1.cpp
@@ -0,0 +1,29 @@
+// By: Gonçalo Leão
+// With contributions by Eduardo Almeida
+
+// Time: O(n^2), Space: O(n)
+int maxSubsequenceBF(int A[], unsigned int n, unsigned int &i, unsigned int &j) {
+
+
+    return 0;
+}
+
+// Alternative, slower solution
+// Time: O(n^3), Space: O(n)
+int maxSubsequenceBF_a(int A[], unsigned int n, unsigned int &i, unsigned int &j) {
+
+    return 0;
+}
+
+
+/// TESTS ///
+#include <gtest/gtest.h>
+
+TEST(TP2_Ex1, maxSubsequence) {
+    int A[] = {-2, 1, -3, 4, -1, 2, 1, -5, 4};
+    unsigned int n = 9;
+    unsigned int i, j;
+    EXPECT_EQ(maxSubsequenceBF(A,n,i,j), 6);
+    EXPECT_EQ(i, 3);
+    EXPECT_EQ(j, 6);
+}
