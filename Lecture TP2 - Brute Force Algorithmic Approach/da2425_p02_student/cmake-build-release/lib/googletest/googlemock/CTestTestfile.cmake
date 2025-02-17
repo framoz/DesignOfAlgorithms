f3fec64477db5b9ca2f238db1d6abee519222dde
framoz <felipe.bueno.ramos@gmail.com>
2025-02-17 08:40:09 +0000

    Lista 2

---
+++ Lecture TP2 - Brute Force Algorithmic Approach/da2425_p02_student/cmake-build-release/lib/googletest/googlemock/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /home/framoz/Documents/FEUP_2425/2s/DA/Lecture TP2 - Brute Force Algorithmic Approach/da2425_p02_student/lib/googletest/googlemock
+# Build directory: /home/framoz/Documents/FEUP_2425/2s/DA/Lecture TP2 - Brute Force Algorithmic Approach/da2425_p02_student/cmake-build-release/lib/googletest/googlemock
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("../googletest")
