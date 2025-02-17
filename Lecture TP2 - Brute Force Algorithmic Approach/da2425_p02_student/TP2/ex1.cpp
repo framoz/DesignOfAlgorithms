0777f900499ad4ca43aa238c35b8197105dc8e64
framoz <felipe.bueno.ramos@gmail.com>
2025-02-17 09:08:48 +0000

    Exercise 1

--- Lecture TP2 - Brute Force Algorithmic Approach/da2425_p02_student/TP2/ex1.cpp
+++ Lecture TP2 - Brute Force Algorithmic Approach/da2425_p02_student/TP2/ex1.cpp
@@ -1,18 +1,42 @@
 // By: Gonçalo Leão
 // With contributions by Eduardo Almeida
 
+#include <climits>
 // Time: O(n^2), Space: O(n)
 int maxSubsequenceBF(int A[], unsigned int n, unsigned int &i, unsigned int &j) {
-
-
-    return 0;
+    int max = INT_MIN;
+    for (int x = 0; x < n; x++) {
+       int temp_sum = 0;
+        for (int y = x; y < n; y++){
+            temp_sum += A[y];
+            if (temp_sum > max) {
+                max = temp_sum;
+                i = x;
+                j = y;
+            }
+        }
+    }
+    return max;
 }
 
 // Alternative, slower solution
 // Time: O(n^3), Space: O(n)
 int maxSubsequenceBF_a(int A[], unsigned int n, unsigned int &i, unsigned int &j) {
-
-    return 0;
+    int max = INT_MIN;
+    for (int x = 0; x < n; x++) {
+        for (int y = x; y < n; y++) {
+            int sum = 0;
+            for (int z = x; z <= y; z++) {
+                sum += A[z];
+                if (sum > max) {
+                    max = sum;
+                    i = x;
+                    j = y;
+                }
+            }
+        }
+    }
+    return max;
 }
 
 
@@ -24,6 +48,15 @@ TEST(TP2_Ex1, maxSubsequence) {
     unsigned int n = 9;
     unsigned int i, j;
     EXPECT_EQ(maxSubsequenceBF(A,n,i,j), 6);
+    EXPECT_EQ(i, 3);
+    EXPECT_EQ(j, 6);
+}
+
+TEST(TP2_Ex1_n3, maxSubsequence) {
+    int A[] = {-2, 1, -3, 4, -1, 2, 1, -5, 4};
+    unsigned int n = 9;
+    unsigned int i, j;
+    EXPECT_EQ(maxSubsequenceBF_a(A,n,i,j), 6);
     EXPECT_EQ(i, 3);
     EXPECT_EQ(j, 6);
 }
