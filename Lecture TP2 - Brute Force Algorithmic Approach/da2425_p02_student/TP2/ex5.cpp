38377a407713f85d40fc3356d2f854c3edb36057
framoz <felipe.bueno.ramos@gmail.com>
2025-02-19 17:29:34 +0000

    Exercise 5

--- Lecture TP2 - Brute Force Algorithmic Approach/da2425_p02_student/TP2/ex5.cpp
+++ Lecture TP2 - Brute Force Algorithmic Approach/da2425_p02_student/TP2/ex5.cpp
@@ -1,9 +1,35 @@
 // By: Gonçalo Leão
 
 
+#include <algorithm>
+#include <iostream>
+
 bool changeMakingBF(unsigned int C[], unsigned int Stock[], unsigned int n, unsigned int T, unsigned int usedCoins[]) {
 
-    return true;
+    for (int i = n-1; i >= 0; i--) {
+        usedCoins[i] = 0;
+        if (T==0) continue;
+        unsigned int n_coins = T / C[i];
+        // std::cout
+        //     << "it:" <<i << "\n"
+        //     << "Start T: " << T << "\n"
+        //     << "Stock: " << Stock[i] << "\n"
+        //     << "n_coins: " << n_coins << "\n";
+
+
+        //if (n_coins == 0) continue;
+        n_coins = (n_coins > Stock[i]) ? Stock[i] : n_coins;
+        //std::cout <<"n_coins after: "<< n_coins << "\n";
+        T -= n_coins * C[i];
+        //std::cout << "Final T: " << T << "\n";
+        Stock[i] -= n_coins;
+        //std::cout << "Final Stock: " << Stock[i] << "\n";
+        usedCoins[i] = n_coins;
+        //std::cout << "Used Coins[" <<i <<"]: " << usedCoins[i] << "\n\n\n";
+
+    }
+    if (T==0) return true;
+    return false;
 }
 
 
@@ -11,6 +37,29 @@ bool changeMakingBF(unsigned int C[], unsigned int Stock[], unsigned int n, unsi
 // Time: O(D*S), Space: O(D), where D is the number of coin values and S is the maximum amount of stock of any value
 bool changeMakingGR(unsigned int C[], unsigned int Stock[], unsigned int n, unsigned int T, unsigned int usedCoins[]) {
 
+    for (int i = n-1; i >= 0; i--) {
+        usedCoins[i] = 0;
+        if (T==0) continue;
+        unsigned int n_coins = T / C[i];
+        // std::cout
+        //     << "it:" <<i << "\n"
+        //     << "Start T: " << T << "\n"
+        //     << "Stock: " << Stock[i] << "\n"
+        //     << "n_coins: " << n_coins << "\n";
+
+
+        //if (n_coins == 0) continue;
+        n_coins = (n_coins > Stock[i]) ? Stock[i] : n_coins;
+        //std::cout <<"n_coins after: "<< n_coins << "\n";
+        T -= n_coins * C[i];
+        //std::cout << "Final T: " << T << "\n";
+        Stock[i] -= n_coins;
+        //std::cout << "Final Stock: " << Stock[i] << "\n";
+        usedCoins[i] = n_coins;
+        //std::cout << "Used Coins[" <<i <<"]: " << usedCoins[i] << "\n\n\n";
+
+    }
+    if (T==0) return true;
     return false;
 }
 
@@ -31,7 +80,7 @@ TEST(TP2_Ex5, hasBFChangeCanonical) {
     unsigned int Stock[] = {1,1,1,1};
     unsigned int n = 4;
     unsigned int usedCoins[4];
-
+    //10x1, 5x0, 2x1, 1x1
     EXPECT_EQ(changeMakingBF(C,Stock,n,13,usedCoins), true);
     EXPECT_EQ(usedCoins[0], 1);
     EXPECT_EQ(usedCoins[1], 1);
