1758f971d02860044a50d74323e76c89c5e446c3
framoz <felipe.bueno.ramos@gmail.com>
2025-02-21 08:42:36 +0000

    Lec 3 unzip

---
+++ Lecture TP3 - Greedy Algorithms/da2425_p03_dist/da2425_p03_dist/TP3/ex6.cpp
@@ -0,0 +1,59 @@
+#include "../data_structures/Graph.h"
+#include <limits>
+
+// Function to perform DFS and find an augmenting path
+template <class T>
+bool dfsFindAugmentingPath(Vertex<T>* v, Vertex<T>* t, double& flow) {
+    if (v == t) return true;
+    v->setVisited(true);
+
+     // TO DO
+
+    return false;
+}
+
+// Ford-Fulkerson algorithm
+template <class T>
+void fordFulkerson(Graph<T>* g, int source, int target) {
+    Vertex<T>* s = g->findVertex(source);
+    Vertex<T>* t = g->findVertex(target);
+
+    if (s == nullptr || t == nullptr || s == t)
+        throw std::logic_error("Invalid source and/or target vertex");
+
+    // TO DO
+
+}
+
+/// TESTS ///
+#include <gtest/gtest.h>
+
+TEST(Algorithm_2, test_fordFulkerson) {
+    Graph<int> myGraph;
+
+    for(int i = 1; i <= 6; i++)
+        myGraph.addVertex(i);
+
+    myGraph.addEdge(1, 2, 3);
+    myGraph.addEdge(1, 3, 2);
+    myGraph.addEdge(2, 5, 4);
+    myGraph.addEdge(2, 4, 3);
+    myGraph.addEdge(2, 3, 1);
+    myGraph.addEdge(3, 5, 2);
+    myGraph.addEdge(4, 6, 2);
+    myGraph.addEdge(5, 6, 3);
+
+    fordFulkerson(&myGraph, 1, 6);
+
+    std::stringstream ss;
+    for(auto v : myGraph.getVertexSet()) {
+        ss << v->getInfo() << "-> (";
+        for (const auto e : v->getAdj())
+            ss << (e->getDest())->getInfo() << "[Flow: " << e->getFlow() << "] ";
+        ss << ") || ";
+    }
+
+    std::cout << ss.str() << std::endl << std::endl;
+
+    EXPECT_EQ("1-> (2[Flow: 3] 3[Flow: 2] ) || 2-> (5[Flow: 1] 4[Flow: 2] 3[Flow: 0] ) || 3-> (5[Flow: 2] ) || 4-> (6[Flow: 2] ) || 5-> (6[Flow: 3] ) || 6-> () || ", ss.str());
+}
