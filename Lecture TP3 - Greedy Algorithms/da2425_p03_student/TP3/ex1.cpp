e1ee7f825fabeee915d3d1501efff5626c66fa0c
framoz <felipe.bueno.ramos@gmail.com>
2025-02-24 16:35:46 +0000

    Corrected wrong tests on exercise 1

--- Lecture TP3 - Greedy Algorithms/da2425_p03_student/TP3/ex1.cpp
+++ Lecture TP3 - Greedy Algorithms/da2425_p03_student/TP3/ex1.cpp
@@ -46,47 +46,6 @@ std::vector<Vertex<T> *> prim(Graph<T> *g) {
 #include <gtest/gtest.h>
 #include <chrono>
 
-TEST(TP3_Ex5, test_prim) {
-    Graph<int> graph = createMSTTestGraph<int>();
-    std::vector<Vertex<int> *> res = prim(&graph);
-
-    std::stringstream ss;
-    for(const auto v : res) {
-        ss << v->getInfo() << "<-";
-        if ( v->getPath() != nullptr ) {
-            ss << v->getPath()->getOrig()->getInfo();
-        }
-        ss << "|";
-    }
-    std::cout << ss.str() << std::endl;
-
-    EXPECT_TRUE(isSpanningTree(res));
-    EXPECT_EQ(spanningTreeCost(res), 11);
-}
-
-TEST(TP3_Ex5, test_performance_prim) {
-    //Change these const parameters as needed
-    const int MIN_SIZE = 10;
-    const int MAX_SIZE = 30; //Try with 100
-    const int STEP_SIZE = 10;
-    const int N_REPETITIONS = 50;
-    for (int n = MIN_SIZE; n <= MAX_SIZE; n += STEP_SIZE) {
-        Graph<int> g;
-        generateRandomGridGraph(n, g);
-        auto start = std::chrono::high_resolution_clock::now();
-        for (int i = 1; i <= N_REPETITIONS; i++)
-            prim(&g);
-        auto finish = std::chrono::high_resolution_clock::now();
-        auto elapsed = std::chrono::duration_cast<std::chrono::milliseconds>(finish - start).count();
-        std::cout << "Processing grid (Prim) " << n << " x " << n << " average time (milliseconds)=" << (elapsed / N_REPETITIONS) << std::endl;
-    }
-}
-
-
-/// TESTS ///
-#include <gtest/gtest.h>
-#include <chrono>
-
 TEST(TP3_Ex1, test_prim) {
     Graph<int> graph = createMSTTestGraph<int>();
     std::vector<Vertex<int> *> res = prim(&graph);
